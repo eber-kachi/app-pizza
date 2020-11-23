886882fcd6b4ce49dccab7f1ccf2674d1b29c1aa
eberKachi <eber.nina96@gmail.com>
2020-11-23 09:31:58 -0400

    agregando clases nuevas

---
+++ pedidoPizza/PizzaBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pedidoPizza
+{
+   public abstract class PizzaBuilder
+   {
+        private Pizza pizza { get; set; }
+        public PrecioCalculator precioCalculator { get; set; }
+        //trabajando 
+
+    }
+}
